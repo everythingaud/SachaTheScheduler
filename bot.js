acb37569da53a3e3c27e31a161973446fc3ef8b1
everuthingaud <setiadar@bc.edu>
2018-09-16 17:53:11 -0400

    testing dialogFlow integration

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+var axios = require('axios');
+
 // To manage Slack interactions
 var { CLIENT_EVENTS, RTM_EVENTS, RtmClient, WebClient } = require('@slack/client');
 var rtm = new RtmClient(process.env.BOT_TOKEN);
@@ -15,5 +17,20 @@ rtm.on(RTM_EVENTS.MESSAGE, (msg) => {
     console.log(msg.channel);
     return;
 });
+
+axios.get('https://api.dialogflow.com/v1/query', {
+      params: {
+        v: 20150910,
+        lang: 'en',
+        timezone: '2017-07-17T14:01:03-0700',
+        query: "Schedule a meeting with someone at 5pm on Friday",
+        sessionId: 12345
+      },
+      headers: {
+        Authorization: 'Bearer 8eb8bfe705ec42bda62581eb11993637'
+      }
+}).then(res => {
+    console.dir(res.data, {depth:null});
+})
 
 rtm.start();
